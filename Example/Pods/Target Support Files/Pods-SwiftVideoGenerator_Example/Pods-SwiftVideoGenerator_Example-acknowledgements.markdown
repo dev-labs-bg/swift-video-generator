fd7ade42b2ccaeca9585142a672c0f29ec956f7f
Teddy Georgieva <teodora.georgieva.92@gmail.com>
2017-10-27 10:57:34 +0300

    Implemented SwiftVideoGenerator pod. Added example project

--- Example/Pods/Target Support Files/Pods-SwiftVideoGenerator_Example/Pods-SwiftVideoGenerator_Example-acknowledgements.markdown
+++ Example/Pods/Target Support Files/Pods-SwiftVideoGenerator_Example/Pods-SwiftVideoGenerator_Example-acknowledgements.markdown
@@ -3,7 +3,7 @@ This application makes use of the following third party libraries:
 
 ## SwiftVideoGenerator
 
-Copyright (c) 2017 teodora.georgieva.92@gmail.com <teodora.georgieva.92@gmail.com>
+Copyright (c) 2017 Dev Labs <http://devlabs.bg/>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- Example/SwiftVideoGenerator/ViewController.swift
+++ Example/SwiftVideoGenerator/ViewController.swift
@@ -7,18 +7,144 @@
 //
 
 import UIKit
+import SwiftVideoGenerator
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  
+  // MARK: - Singleton properties
+  
+  // MARK: - Static properties
+  
+  // MARK: - Public properties
+  
+  @IBOutlet weak var generateSingleVideoButton: UIButton! {
+    didSet {
+      generateSingleVideoButton.accessibilityLabel = "single"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  }
+  
+  @IBOutlet weak var generateMultipleVideoButton: UIButton! {
+    didSet {
+      generateMultipleVideoButton.accessibilityLabel = "multiple"
     }
-
+  }
+  
+  @IBOutlet weak var mergeVideosButton: UIButton! {
+    didSet {
+      mergeVideosButton.accessibilityLabel = "merge"
+    }
+  }
+  
+  // MARK: - Public methods
+  
+  @IBAction func generateSingleVideoButtonClickHandler(_ sender: UIButton) {
+    if let audioURL4 = Bundle.main.url(forResource: "audio4", withExtension: "mp3"), let _image1 = UIImage(named: "image4") {
+      
+      if let videoGenerator = VideoGenerator(withImages: [_image1], andAudios: [audioURL4], andType: .single) {
+        videoGenerator.scaleWidth = 800
+        videoGenerator.fileName = "singleMovie"
+        videoGenerator.videoBackgroundColor = .black
+        
+        videoGenerator.generate({ (progress) in
+          print(progress)
+        }, success: { (url) in
+          print(url)
+          self.createAlertView(message: "Finished single type video generation")
+        }) { (error) in
+          self.createAlertView(message: error.description)
+          print(error)
+        }
+      }
+    }
+  }
+  
+  @IBAction func generateMultipleVideoButtonClickHandler(_ sender: UIButton) {
+    if let audioURL1 = Bundle.main.url(forResource: "audio1", withExtension: "mp3"), let audioURL2 = Bundle.main.url(forResource: "audio2", withExtension: "mp3"), let audioURL3 = Bundle.main.url(forResource: "audio3", withExtension: "mp3") {
+      if let _image1 = UIImage(named: "image1"), let _image2 = UIImage(named: "image2"), let _image3 = UIImage(named: "image3") {
+        
+        if let videoGenerator1 = VideoGenerator(withImages: [_image1, _image2, _image3], andAudios: [audioURL1, audioURL2, audioURL3], andType: .multiple) {
+          videoGenerator1.fileName = "multipleVideo"
+          videoGenerator1.videoBackgroundColor = .black
+          
+          videoGenerator1.generate({ (progress) in
+            print(progress)
+          }, success: { (videoURL) in
+            print(videoURL)
+            self.createAlertView(message: "Finished multiple type video generation")
+          }, failure: { (error) in
+            self.createAlertView(message: error.description)
+            print(error)
+          })
+        }
+      }
+    }
+  }
+  
+  @IBAction func mergeVideosButtonClickHandler(_ sender: UIButton) {
+    //    if let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+    //      if let paths = try? FileManager.default.contentsOfDirectory(atPath: documentsPath) {
+    //
+    //        var videoURLs: [URL] = []
+    //        for path in paths {
+    //          videoURLs.append(URL(fileURLWithPath: documentsPath).appendingPathComponent(path))
+    //        }
+    //
+    //        VideoGenerator.mergeMovies(videoURLs: videoURLs, andFileName: "mergedMovie", success: { (videoURL) in
+    //          self.createAlertView(message: "Finished merging videos")
+    //          print(videoURL)
+    //        }) { (error) in
+    //          print(error)
+    //          self.createAlertView(message: error.description)
+    //        }
+    //      }
+    //    }
+    
+    if let videoURL1 = Bundle.main.url(forResource: "video1", withExtension: "mov"), let videoURL2 = Bundle.main.url(forResource: "video2", withExtension: "mov") {
+      VideoGenerator.mergeMovies(videoURLs: [videoURL1, videoURL2], andFileName: "mergedMovie", success: { (videoURL) in
+        self.createAlertView(message: "Finished merging videos")
+        print(videoURL)
+      }) { (error) in
+        print(error)
+        self.createAlertView(message: error.description)
+      }
+    }
+  }
+  
+  // MARK: - Initialize/Livecycle methods
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - Override methods
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    
+    generateSingleVideoButton.layer.cornerRadius = generateSingleVideoButton.frame.height / 2
+    generateMultipleVideoButton.layer.cornerRadius = generateMultipleVideoButton.frame.height / 2
+    mergeVideosButton.layer.cornerRadius = mergeVideosButton.frame.height / 2
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  // MARK: - Private properties
+  
+  // MARK: - Private methods
+  
+  /**
+   Create and show an alert view
+   */
+  fileprivate func createAlertView(message: String?) {
+    let messageAlertController = UIAlertController(title: "message", message: message, preferredStyle: .alert)
+    let buttonTitle = "OK"
+    
+    messageAlertController.addAction(UIAlertAction(title: buttonTitle, style: .default, handler: { (action: UIAlertAction!) in
+      messageAlertController.dismiss(animated: true, completion: nil)
+    }))
+    
+    present(messageAlertController, animated: true, completion: nil)
+  }
 }
-
