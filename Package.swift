0ecac92ffd10d4dde30b0a5f096d373d7403b645
Steliyan Hadzhidenev <hadzhidenev@gmail.com>
2019-11-22 15:28:08 +0200

    Changed the path of the source files

--- Package.swift
+++ Package.swift
@@ -19,8 +19,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "swift-video-generator",
-            dependencies: []),
+            name: "SwiftVideoGenerator",
+            dependencies: [],
+            path: "Sources/**/*"),
         .testTarget(
             name: "swift-video-generatorTests",
             dependencies: ["swift-video-generator"]),
