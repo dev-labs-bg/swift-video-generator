a53a9e42742251a9f2ceaa54a201cb28eadd7276
Steliyan Hadzhidenev <hadzhidenev@gmail.com>
2019-11-22 15:30:01 +0200

    Fixed the path component

--- Package.swift
+++ Package.swift
@@ -20,8 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftVideoGenerator",
-            dependencies: [],
-            path: "Sources/**/*"),
+            dependencies: []),
         .testTarget(
             name: "swift-video-generatorTests",
             dependencies: ["swift-video-generator"]),
