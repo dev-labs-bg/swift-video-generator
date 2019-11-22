510f8af06cc6f03abf5b94cfe5d128481df83cbb
Steliyan Hadzhidenev <hadzhidenev@gmail.com>
2019-11-22 15:37:27 +0200

    Fixed Package dependencies

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "swift-video-generator",
-            targets: ["swift-video-generator"]),
+            targets: ["SwiftVideoGenerator"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,6 +23,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "swift-video-generatorTests",
-            dependencies: ["swift-video-generator"]),
+            dependencies: ["SwiftVideoGenerator"]),
     ]
 )
